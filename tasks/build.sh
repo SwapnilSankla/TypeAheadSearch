6ea701008583aadf5a373c95488810ad6f664b0a
Swapnil Sankla <swapnil.sankla@gmail.com>
2018-04-20 18:36:46 +0530

    Specify full path for pod in build script

--- tasks/build.sh
+++ tasks/build.sh
@@ -1,2 +1,2 @@
-pod install
+/usr/local/bin/pod install
 xcodebuild -workspace TypeAheadSearch.xcworkspace -scheme "TypeAheadSearch" -destination 'platform=iOS Simulator,OS=11.3,name=iPhone 6s' clean build
