eb36778d7c969f7a39e193259235e97043993b3c
Swapnil Sankla <swapnil.sankla@gmail.com>
2018-04-26 19:00:36 +0530

    Let me zip my app

--- tasks/build.sh
+++ tasks/build.sh
@@ -1,2 +1,3 @@
 /usr/local/bin/pod install
 xcodebuild -workspace TypeAheadSearch.xcworkspace -scheme "TypeAheadSearch" -destination 'platform=iOS Simulator,OS=11.3,name=iPhone 6s' -archivePath clean build -derivedDataPath build
+zip -r -X build/TypeAheadSearch.zip build/Products/Debug-iphonesimulator/TypeAheadSearch.App
