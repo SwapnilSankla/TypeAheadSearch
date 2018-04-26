1c67926695e9dc22cbc80ab4db2ee170e99e1c95
Swapnil Sankla <swapnil.sankla@gmail.com>
2018-04-26 19:05:32 +0530

    Correct path for iOS SDK 11.3

--- tasks/build.sh
+++ tasks/build.sh
@@ -1,3 +1,3 @@
 /usr/local/bin/pod install
 xcodebuild -workspace TypeAheadSearch.xcworkspace -scheme "TypeAheadSearch" -destination 'platform=iOS Simulator,OS=11.3,name=iPhone 6s' -archivePath clean build -derivedDataPath build
-zip -r -X build/TypeAheadSearch.zip build/Products/Debug-iphonesimulator/TypeAheadSearch.App
+zip -r -X build/TypeAheadSearch.zip build/Build/Products/Debug-iphonesimulator/TypeAheadSearch.App
