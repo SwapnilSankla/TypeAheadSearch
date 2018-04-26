1de9085f0ea5d6b854b9923b87b60fe12439c995
Swapnil Sankla <swapnil.sankla@gmail.com>
2018-04-26 17:06:01 +0530

    Set output path

--- tasks/build.sh
+++ tasks/build.sh
@@ -1,2 +1,2 @@
 /usr/local/bin/pod install
-xcodebuild -workspace TypeAheadSearch.xcworkspace -scheme "TypeAheadSearch" -destination 'platform=iOS Simulator,OS=11.3,name=iPhone 6s' clean build
+xcodebuild -workspace TypeAheadSearch.xcworkspace -scheme "TypeAheadSearch" -destination 'platform=iOS Simulator,OS=11.2,name=iPhone 6s' -archivePath clean build -derivedDataPath build
