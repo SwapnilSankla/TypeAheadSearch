94439bc9a6ea29f93acead9744d47a0194c89078
Swapnil Sankla <swapnil.sankla@gmail.com>
2018-04-30 14:04:32 +0530

    No need to specify platform with specific simulator version. Providing sdk is enough

--- tasks/build.sh
+++ tasks/build.sh
@@ -1,3 +1,3 @@
 /usr/local/bin/pod install
-xcodebuild -workspace TypeAheadSearch.xcworkspace -scheme "TypeAheadSearch" -destination 'platform=iOS Simulator,OS=11.3,name=iPad Pro (9.7-inch)' clean build -derivedDataPath build
+xcodebuild -workspace TypeAheadSearch.xcworkspace -scheme "TypeAheadSearch" clean build -derivedDataPath build -sdk iphonesimulator11.2
 zip -r -X build/TypeAheadSearch.zip build/Build/Products/Debug-iphonesimulator/TypeAheadSearch.App
