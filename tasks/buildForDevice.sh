efe5991b1528cfa934cdda42f54ae8de600dd714
Swapnil Sankla <swapnil.sankla@gmail.com>
2018-04-30 14:06:41 +0530

    Build script for device

---
+++ tasks/buildForDevice.sh
@@ -0,0 +1,3 @@
+/usr/local/bin/pod install
+xcodebuild -workspace TypeAheadSearch.xcworkspace -scheme "TypeAheadSearch" clean build -derivedDataPath build -sdk iphoneos11.2
+zip -r -X build/TypeAheadSearch.zip build/Build/Products/Debug-iphonesimulator/TypeAheadSearch.App
